e27528f2bf13fea20266d627ad4be82df755c7ba
Hassan <paidthatmoney@gmail.com>
2024-06-09 14:47:27 +0900

    영우님 작업물 병합 (추가) - 인브라우저 훅 저장하기, 태그 목록 불러오기 - 관련하여 PageDetailsDialog, Sharing 재업로드

--- app/src/main/java/com/hanto/hook/urlHandler/Sharing.kt
+++ app/src/main/java/com/hanto/hook/urlHandler/Sharing.kt
@@ -1,29 +1,21 @@
 package com.hanto.hook.urlHandler
 
+
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider.NewInstanceFactory.Companion.instance
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
-import java.lang.ref.WeakReference
+
 
 class Sharing : AppCompatActivity() {
 
-    companion object {
-        private var instance: WeakReference<Sharing>? = null
-
-        fun getInstance(): Sharing? {
-            return instance?.get()
-        }
-    }
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        instance = WeakReference(this)
         handleIncomingSharedContent()
     }
 
@@ -35,35 +27,47 @@ class Sharing : AppCompatActivity() {
         if (receivedAction == Intent.ACTION_SEND && receivedType == "text/plain") {
             val sharedText: String? = receivedIntent.getStringExtra(Intent.EXTRA_TEXT)
             val url = sharedText
+            println("fetchDataFromUrl 실행")
             url?.let { fetchDataFromUrl(it) }
         }
     }
 
     private fun fetchDataFromUrl(url: String) {
         GlobalScope.launch(Dispatchers.Main) {
-            val title = withContext(Dispatchers.IO) {
-                fetchOpenGraphTitleFromUrl(url)
+            val (title, thumbnailUrl) = withContext(Dispatchers.IO) {
+                println("fetchOpenGraphInfoFromUrl 실행")
+                fetchOpenGraphInfoFromUrl(url)
             }
+            println("openPageDetailsDialog 실행")
             title?.let { openPageDetailsDialog(it, url) }
         }
     }
 
-    private fun fetchOpenGraphTitleFromUrl(url: String): String? {
+    private suspend fun fetchOpenGraphInfoFromUrl(url: String): Pair<String?, String?> {
         var title: String? = null
+        var thumbnailUrl: String? = null
 
         try {
             val document = Jsoup.connect(url).get()
+
+            // Get page title
             val ogTitleElement = document.select("meta[property=og:title]").first()
             title = ogTitleElement?.attr("content")
+
+            // Get thumbnail URL
+            val ogImageElement = document.select("meta[property=og:image]").first()
+            thumbnailUrl = ogImageElement?.attr("content")
         } catch (e: Exception) {
+            // Handle exceptions appropriately, such as showing error messages
             e.printStackTrace()
         }
 
-        return title
+        return Pair(title, thumbnailUrl)
     }
 
     private fun openPageDetailsDialog(title: String, url: String) {
-        val dialog = PageDetailsDialog(this, title, url)
+        val dialog = PageDetailsDialog(this,  title, url)
         dialog.show()
+
     }
 }
